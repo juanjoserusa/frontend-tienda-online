0ee015f3388d4a5cdaafbbc27b05f63101a728fa
Juan Jose Ruiz Sanchez <juanjoserusa@gmail.com>
2022-12-13 13:42:14 +0100

    tienda online front

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { data } from "../data";
+
+export const ProductList = ({ allProducts, setAllProducts, countProducts, setCountProducts, total, setTotal }) => {
+  const onAddProduct = (product) => {
+    if (allProducts.find((item) => item.id === product.id)) {
+      const products = allProducts.map(item =>
+        item.id === product.id 
+        ? { ...item, quantity: item.quantity + 1 } 
+        : item
+      );
+
+      setTotal(total + product.price * product.quantity)
+      setCountProducts(countProducts + product.quantity)
+      return setAllProducts([...products])
+    }
+
+    setTotal(total + product.price * product.quantity)
+    setCountProducts(countProducts + product.quantity)
+    setAllProducts([...allProducts, product]);
+  };
+  console.log(allProducts);
+
+
+  return (
+    <div className="container-items pt-5 mt-5">
+      {data.map((product) => (
+        <div className="item" key={product.id}>
+          <figure>
+            <img src={product.img} alt={product.nameProduct} />
+          </figure>
+          <div className="info-product">
+            <h2>{product.nameProduct}</h2>
+            <p className="price">{product.price} €</p>
+            <button
+              className="btn btn-primary"
+              onClick={() => onAddProduct(product)}
+            >
+              Añadir al carrito
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const data = [
+	{
+		id: 1,
+		img: 'https://m.media-amazon.com/images/I/61AsFcvIBNL._AC_SS450_.jpg',
+		nameProduct: 'Alfombrilla',
+		price: 10,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1099&q=80',
+		nameProduct: 'Reloj',
+		price: 50,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+		nameProduct: 'Smartwatch',
+		price: 90,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: 'https://cdn.businessinsider.es/sites/navi.axelspringer.es/public/media/image/2020/02/raton-gaming-logitech-g502-hero-1871315.jpg?tf=3840x',
+		nameProduct: 'Ratón',
+		price: 30,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+		nameProduct: 'Auriculares',
+		price: 20,
+		quantity: 1,
+	},
+    {
+		id: 6,
+		img: 'https://ae01.alicdn.com/kf/Hbb68d6afd8104b6eae547ca3efd60302l/REDRAGON-Fizz-K617-RGB-Mini-teclado-mec-nico-para-juegos-con-cable-interruptor-rojo-61-teclas.jpg_640x640.jpg',
+		nameProduct: 'Teclado',
+		price: 30,
+		quantity: 1,
+	},
+	// Aquí pueden agregar más o modificar los productos existentes
+];
